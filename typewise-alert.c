992a1b2073aacccbbb4d9b0328d19d5f30c1bec2
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-02-25 01:07:05 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -55,9 +55,6 @@ void Initialize_and_Start_BatteryCheckSystem (AlertTarget currentAlertTarget, Ba
     int RangeBasedOnCoolingType[COOLING_TYPES][TEMPERATURE_LIMITS] = {{PASSIVE_COOLING_LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT}, 
                                                                       {HI_ACTIVE_COOLING_LOWER_LIMIT, HI_ACTIVE_COOLING_UPPER_LIMIT}, 
                                                                       {MED_ACTIVE_COOLING_LOWER_LIMIT, MED_ACTIVE_COOLING_UPPER_LIMIT}};
-    
-    /* Start the System */
-    BreachType breachType = PerformBatteryCheck(batteryChar, temperatureInC, RangeBasedOnCoolingType);
-    
-    (void) alertAndPrint(currentAlertTarget,breachType);
+     
+    (void) alertAndPrint(currentAlertTarget,PerformBatteryCheck(batteryChar, temperatureInC, RangeBasedOnCoolingType));
 }
