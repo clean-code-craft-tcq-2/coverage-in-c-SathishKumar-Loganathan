440242d55960d6523a8dd259ffd312de6e91373a
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-02-25 01:20:03 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
-const void (*pointerToAlertingOptions[]) (BreachType) = {prepareAlertTextForController,prepareAlertTextForEmail};
+void (*pointerToAlertingOptions[]) (BreachType) = {prepareAlertTextForController,prepareAlertTextForEmail};
 char MessageToBeDisplayedOnConsole[44] = "";
-const int RangeBasedOnCoolingType[COOLING_TYPES][TEMPERATURE_LIMITS] = {{PASSIVE_COOLING_LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT}, 
+int RangeBasedOnCoolingType[COOLING_TYPES][TEMPERATURE_LIMITS] = {{PASSIVE_COOLING_LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT}, 
                                                                         {HI_ACTIVE_COOLING_LOWER_LIMIT, HI_ACTIVE_COOLING_UPPER_LIMIT}, 
                                                                         {MED_ACTIVE_COOLING_LOWER_LIMIT, MED_ACTIVE_COOLING_UPPER_LIMIT}};
 
