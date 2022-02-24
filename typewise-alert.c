fb2d0334494a4e19df99b702eb64ffdc72d3a89c
SathishKumar Loganathan <43195344+SathishKumar-Loganathan@users.noreply.github.com>
2022-02-25 00:20:02 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
+char MessageToBeDisplayedOnConsole[44] = "";
+
 BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
   if(value < lowerLimit) {
     return TOO_LOW;
@@ -19,18 +21,18 @@ BreachType classifyTemperatureBreach(int MapForCoolingTypeVsRange[][TEMPERATURE_
   return inferBreach(temperatureInC, lowerLimit, upperLimit);
 }
 
-void prepareAlertTextForController(BreachType breachType, char AlertMessage[10]) {
+void prepareAlertTextForController(BreachType breachType) {
   const unsigned short header = 0xfeed;
-  snprintf(AlertMessage,10,"%x : %x", header, breachType);
+  snprintf(MessageToBeDisplayedOnConsole,10,"%x : %x", header, breachType);
 }
 
-void prepareAlertTextForEmail(BreachType breachType, char AlertMessage[44]) {
+void prepareAlertTextForEmail(BreachType breachType) {
    char recepient[] = "a.b@c.com";
    char AlertMessagesForEmail[][32] = {"Hi, the temperature is Normal",
                                        "Hi, the temperature is too low", 
                                        "Hi, the temperature is too high"};
 
-   snprintf(AlertMessage,44,"%s : %s",recepient,&(AlertMessagesForEmail[breachType][0]));
+   snprintf(MessageToBeDisplayedOnConsole,44,"%s : %s",recepient,&(AlertMessagesForEmail[breachType][0]));
 }
 
 void printOnConsole(char *stringToBePrinted) {
@@ -43,8 +45,7 @@ BreachType PerformBatteryCheck(BatteryCharacter batteryChar, double temperatureI
 
 void Initialize_and_Start_BatteryCheckSystem (AlertTarget currentAlertTarget, BatteryCharacter batteryChar, double temperatureInC) {
     /* Initialize parameters*/
-    char MessageToBeDisplayedOnConsole[44] = "";
-    void (*pointerToAlertingOptions[]) (BreachType, char *) = {prepareAlertTextForController,prepareAlertTextForEmail};
+    void (*pointerToAlertingOptions[]) (BreachType) = {prepareAlertTextForController,prepareAlertTextForEmail};
     int RangeBasedOnCoolingType[COOLING_TYPES][TEMPERATURE_LIMITS] = {{PASSIVE_COOLING_LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT}, 
                                                                       {HI_ACTIVE_COOLING_LOWER_LIMIT, HI_ACTIVE_COOLING_UPPER_LIMIT}, 
                                                                       {MED_ACTIVE_COOLING_LOWER_LIMIT, MED_ACTIVE_COOLING_UPPER_LIMIT}};
@@ -52,7 +53,9 @@ void Initialize_and_Start_BatteryCheckSystem (AlertTarget currentAlertTarget, Ba
     /* Start the System */
     BreachType breachType = PerformBatteryCheck(batteryChar, temperatureInC, RangeBasedOnCoolingType);
     
+    MessageToBeDisplayedOnConsole = ""; // Clear the Buffer
+    
     /* Provide Alert on the requested Target*/
-    pointerToAlertingOptions[currentAlertTarget](breachType, MessageToBeDisplayedOnConsole);
+    pointerToAlertingOptions[currentAlertTarget](breachType);
     printOnConsole(MessageToBeDisplayedOnConsole);
 }
